9d033bd017d043496c7f799d04bab78811ee6f06
Kaspar Manz <kaspar.manz@nzz.ch>
2025-04-16 19:00:17 +0200

    wip: first library bundle

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,20 @@
+import {dirname, resolve} from 'node:path'
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { fileURLToPath } from 'node:url';
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [svelte()],
+  build: {
+    lib: {
+      entry: {
+        'svelte': resolve(__dirname, 'src/ScrollyVideo.svelte'),
+      },
+      name: 'ScrollyVideo',
+    }
+
+  }
 })
