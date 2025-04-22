b8989571a44ea085cba5a2bca440ba91c1b52ada
Kaspar Manz <kaspar.manz@nzz.ch>
2025-04-22 14:59:45 +0200

    chore: add vite vue plugin and update/remove unused dependencies

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,31 @@
-import {dirname, resolve} from 'node:path'
-import { defineConfig } from 'vite'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { dirname, resolve } from 'node:path';
+import { defineConfig } from 'vite';
+import { svelte } from '@sveltejs/vite-plugin-svelte';
+import vue from '@vitejs/plugin-vue';
 import { fileURLToPath } from 'node:url';
 
-const __dirname = dirname(fileURLToPath(import.meta.url))
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+const libraryBuild = {
+  lib: {
+    entry: {
+      svelte: resolve(__dirname, 'src/ScrollyVideo.svelte'),
+      react: resolve(__dirname, 'src/ScrollyVideo.jsx'),
+      vue: resolve(__dirname, 'src/ScrollyVideo.vue'),
+      ScrollyVideo: resolve(__dirname, 'src/ScrollyVideo.js'),
+    },
+    name: 'ScrollyVideo',
+  },
+  rollupOptions: {
+    external: ['svelte', 'vue', 'mp4boxjs', 'react', 'ua-parser-js'],
+  },
+};
+
+const docsBuild = {};
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [svelte()],
-  build: {
-    lib: {
-      entry: {
-        'svelte': resolve(__dirname, 'src/ScrollyVideo.svelte'),
-      },
-      name: 'ScrollyVideo',
-    }
-
-  }
-})
+export default defineConfig(({ mode }) => ({
+  base: './',
+  plugins: [svelte(), vue()],
+  build: mode === 'library' ? libraryBuild : docsBuild,
+}));
