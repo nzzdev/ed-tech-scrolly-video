8192d7974496c2558e7b231dde1c95867eeb2e2e
Kaspar Manz <kaspar.manz@nzz.ch>
2025-04-23 12:51:54 +0200

    docs: add commentary to vite config

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,12 @@
+/**
+ * @fileOverview Vite configuration, used for both the library and the docs site.
+ * The docs site can be used to test and develop the library.
+ *
+ * - `npm run dev` or `npm run start`: start the preview of the docs site.
+ * - `npm run build`: build the library.
+ * - `npm run build-docs`: build the docs site.
+ */
+
 import { dirname, resolve } from 'node:path';
 import { defineConfig } from 'vite';
 import { svelte } from '@sveltejs/vite-plugin-svelte';
@@ -6,10 +15,16 @@ import { fileURLToPath } from 'node:url';
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
+/**
+ * Settings for building the library.
+ *
+ * React and Vue files are kept because they have been part of the original code,
+ * in case we want to merge these changes back upstream.
+ */
 const libraryBuild = {
   lib: {
     entry: {
-      "scrolly-video": resolve(__dirname, 'src/ScrollyVideo.js'),
+      'scrolly-video': resolve(__dirname, 'src/ScrollyVideo.js'),
       svelte: resolve(__dirname, 'src/ScrollyVideo.svelte'),
       react: resolve(__dirname, 'src/ScrollyVideo.jsx'),
       vue: resolve(__dirname, 'src/ScrollyVideo.vue'),
@@ -21,19 +36,31 @@ const libraryBuild = {
   },
 };
 
+/**
+ * No special settings are needed for the docs site.
+ */
 const docsBuild = {};
 
-const docsBuildPlugins = [svelte(), vue()]
-const libraryBuildPlugins = [viteWrapCodeInIIFE()]
+const docsBuildPlugins = [svelte(), vue()];
+const libraryBuildPlugins = [viteWrapCodeInIIFE()];
 
-// https://vite.dev/config/
+/**
+ * @see https://vite.dev/config/
+ */
 export default defineConfig(({ mode }) => ({
   base: './',
-  plugins: mode === 'library' ? libraryBuildPlugins.concat(docsBuildPlugins) : docsBuildPlugins,
+  plugins:
+    mode === 'library'
+      ? libraryBuildPlugins.concat(docsBuildPlugins)
+      : docsBuildPlugins,
   build: mode === 'library' ? libraryBuild : docsBuild,
 }));
 
-
+/**
+ * Solves the issue of vite not wrapping all the code in an IIFE, which can lead to bundles overriding each other.
+ * @see https://github.com/vitejs/vite/issues/16443
+ * @see https://github.com/vitejs/vite/issues/17608#issue-2388013526
+ */
 function viteWrapCodeInIIFE(options = {}) {
   return {
     name: 'vite-wrap-code-in-iife',
@@ -41,7 +68,11 @@ function viteWrapCodeInIIFE(options = {}) {
     enforce: 'post',
     generateBundle(outputOptions, bundle) {
       for (const [fileName, chunkOrAsset] of Object.entries(bundle)) {
-        if (chunkOrAsset.type === 'chunk' && options.files && options.files.includes(fileName)) {
+        if (
+          chunkOrAsset.type === 'chunk' &&
+          options.files &&
+          options.files.includes(fileName)
+        ) {
           chunkOrAsset.code = `(function () {${chunkOrAsset.code}})();`;
         }
       }
